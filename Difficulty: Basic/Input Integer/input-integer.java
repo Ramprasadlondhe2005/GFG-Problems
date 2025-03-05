85823cbe9fa2c5eaced5ab3538279bc481925b42
Ramprasadlondhe2005 <ramprasadlondhe24@gmail.com>
2025-03-05 23:44:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Input Integer/input-integer.java
@@ -0,0 +1,26 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+import java.util.*;
+
+
+class GFG {
+    public static void main(String args[]) {
+        // Implement Scanner Class
+        //  Take input
+        //  Add 10 and Print
+        
+        Scanner Sc = new Scanner(System.in);
+        int num=0;
+         num=Sc.nextInt();
+        
+        int res=num+10;
+        
+        System.out.println(res);
+    }
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
