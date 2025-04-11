522b9c21298e5ba334dd3b5fb984b27def5cde12
Ramprasadlondhe2005 <ramprasadlondhe24@gmail.com>
2025-04-12 00:52:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Without Newline/print-without-newline.java
@@ -0,0 +1,20 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        // Print in the required format without new Line
+        
+        System.out.println(a+" "+b);
+    }
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
