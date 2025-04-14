7c59c058489097436fee47fd4ada0eab356ac06a
Ramprasadlondhe2005 <ramprasadlondhe24@gmail.com>
2025-04-14 16:39:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/While Loop/while-loop.java
@@ -0,0 +1,23 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        // Your Code Here
+        Scanner sc = new Scanner(System.in);
+        int x=sc.nextInt();
+        
+        while(x>=0)
+        {
+            System.out.print(x+" ");
+            x--;
+        }
+    }
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
