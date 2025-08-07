2df2b97708dee02596535eb6a571c73a61ac39b4
Ramprasadlondhe2005 <ramprasadlondhe24@gmail.com>
2025-08-08 00:29:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Binary Search/binary-search.java
+++ Difficulty: Easy/Binary Search/binary-search.java
@@ -1,65 +1,28 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-public class GFG {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        sc.nextLine(); // consume the newline
-        while (t-- > 0) {
-            int k = sc.nextInt();
-            sc.nextLine(); // consume the newline
-            String input = sc.nextLine();
-            String[] strNumbers = input.split(" ");
-            int[] arr = new int[strNumbers.length];
-            for (int i = 0; i < strNumbers.length; i++) {
-                arr[i] = Integer.parseInt(strNumbers[i]);
-            }
-            Solution ob = new Solution();
-            int res = ob.binarysearch(arr, k);
-            System.out.println(res);
-
-            System.out.println("~");
-        }
-        sc.close();
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
     public int binarysearch(int[] arr, int k) {
-        // Code Here
         
-        int low=0;
-        int high=arr.length-1;
-        int result=-1;
-        
-        while(low<=high)
+        int start = 0, end = arr.length - 1;
+        int result = -1;  
+
+        while (start <= end) 
         {
-            int mid=low+(high-low)/2;
-              
-              
-            if(arr[mid]==k)
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == k) 
             {
-                result= mid;
-                high = mid - 1;  
-            }
-            else if(arr[mid]<k) 
+                result = mid;        
+                end = mid - 1;
+            } 
+            else if (arr[mid] < k) 
             {
-                low=mid+1;
-            }
+                start = mid + 1;
+            } 
             else 
             {
-                high=mid-1;
+                end = mid - 1;
             }
         }
+
         return result;
     }
 }
