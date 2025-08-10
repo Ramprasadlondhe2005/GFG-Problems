9d9b50c2ea87f19b1344a1d40348fa3b3bdf4c7c
Ramprasadlondhe2005 <ramprasadlondhe24@gmail.com>
2025-08-10 15:46:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.java
+++ Difficulty: Easy/Second Largest/second-largest.java
@@ -3,8 +3,10 @@ class Solution {
         // code here
         Arrays.sort(arr);
         
-        for(int i = arr.length-2 ; i>=0;i--){
-            if(arr[i] != arr[arr.length-1]){
+        for(int i = arr.length-2 ; i>=0;i--)
+        {
+            if(arr[i] != arr[arr.length-1])
+            {
                 return arr[i];
             }
         }
