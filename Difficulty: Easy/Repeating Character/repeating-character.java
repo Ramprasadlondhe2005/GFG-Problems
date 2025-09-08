e47203b48f9541e1419373ad7f4c84e37659dcf7
Ramprasadlondhe2005 <ramprasadlondhe24@gmail.com>
2025-09-09 02:19:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Repeating Character/repeating-character.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int repeatedCharacter(String S) {
+        // code here
+        
+        
+        for(int i=0;i<S.length();i++){
+            
+            char ch = S.charAt(i);
+            
+            if(S.indexOf(ch) != S.lastIndexOf(ch)){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
