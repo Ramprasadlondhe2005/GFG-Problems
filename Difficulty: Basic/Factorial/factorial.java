1649b783e07ae438bcb76f2fb9de2305c0c912aa
Ramprasadlondhe2005 <ramprasadlondhe24@gmail.com>
2025-09-14 01:20:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Factorial/factorial.java
+++ Difficulty: Basic/Factorial/factorial.java
@@ -1,40 +1,13 @@
-//{ Driver Code Starts
-//Initial Template for Java
-
-import java.util.Scanner;
-
-class Main {
-
-    
-// } Driver Code Ends
-
-//User function Template for Java
-
-public static int nFactorial(int n){
-    int ans = 1;
-    
-    //Write your code here
-    if(n == 0) return 1;
-    
-     return ans = n * nFactorial(n-1);
-   
-}
-
-
-//{ Driver Code Starts.
-
-    public static void main(String[] args)
-    {
-        Scanner scn = new Scanner(System.in);
-        int t = scn.nextInt();
-        while(t-- > 0) {
-            int n = scn.nextInt();
-            int ans = nFactorial(n);
-            System.out.println(ans);
+class Solution {
+    // Function to calculate factorial of a number.
+    int factorial(int n) {
+        // code here
         
-System.out.println("~");
-}
-        scn.close();
+        int fact = 1;
+        for(int i=1;i<=n;i++){
+            fact = fact * i ;
+        }
+        
+        return fact;
     }
 }
-// } Driver Code Ends
